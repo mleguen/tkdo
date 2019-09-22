b7d0e2cf2a5c115e559c020aa3cd0aa0fc000350
Mael Le Guen <github@mlg.lgnby.fr>
2019-09-22 20:43:39 +0200

    front: initialisation routing et bootstrap

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { replace } from 'feather-icons';
+
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'front';
+export class AppComponent implements OnInit {
+  public titre: string = environment.titre;
+
+  public constructor(private titleService: Title) { }
+
+  public ngOnInit() {
+    // Librairies tierces
+    replace();
+    
+    this.titleService.setTitle(this.titre);
+  }
 }
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent
   ],
   imports: [
+    NgbModule,
     BrowserModule,
     AppRoutingModule
   ],
