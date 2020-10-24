c95733b0f2cdfe525a3b8c30cfd8dfc98d388903
Mael Le Guen <github@mlg.lgnby.fr>
2020-10-24 02:10:21 +0200

    chore(front): montées de version bootstrap et moment

--- front/e2e/src/app.e2e-spec.ts
+++ front/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     page.navigateTo();
-    expect(await page.getTitleText()).toEqual('Tirage cadeaux');
+    expect(await page.getTitleText()).toEqual('Connexion');
   });
 
   afterEach(async () => {
