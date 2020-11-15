1295e260f50391a71e50b6feec0996eb14b6558f
mleguen <github@mlg.lgnby.fr>
2020-11-15 18:25:43 +0100

    chore(ngskel): ne dépend plus d'une installation globale de ng

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tkdo'`, () => {
+  it(`should have as title 'tkdo-front'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tkdo');
+    expect(app.title).toEqual('tkdo-front');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('tkdo app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('tkdo-front app is running!');
   });
 });
--- init-ng.sh
+++ init-ng.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 rm -rf front
-ng new --directory front --routing --skip-git --style scss tkdo
+npm install
+./node_modules/.bin/ng new --directory front --routing --skip-git --style scss tkdo-front
 cd front
-ng generate component connexion
-ng generate component deconnexion
-ng generate component header
-ng generate component liste-idees
-ng generate component occasion
-ng generate component profil
-ng generate interceptor auth-backend
-ng generate service backend
-ng generate guard --implements CanActivate connexion
-ng generate interceptor dev-backend
-ng generate interceptor erreur-backend
+./node_modules/.bin/ng generate component connexion
+./node_modules/.bin/ng generate component deconnexion
+./node_modules/.bin/ng generate component header
+./node_modules/.bin/ng generate component liste-idees
+./node_modules/.bin/ng generate component occasion
+./node_modules/.bin/ng generate component profil
+./node_modules/.bin/ng generate interceptor auth-backend
+./node_modules/.bin/ng generate service backend
+./node_modules/.bin/ng generate guard --implements CanActivate connexion
+./node_modules/.bin/ng generate interceptor dev-backend
+./node_modules/.bin/ng generate interceptor erreur-backend
